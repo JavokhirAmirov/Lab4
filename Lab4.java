43d40f390104d2b62eb128def6a45eb4be869f40
JavokhirAmirov <36505258+JavokhirAmirov@users.noreply.github.com>
2018-11-29 00:46:29 +0200

    Update Lab4.java

--- Lab4.java
+++ Lab4.java
@@ -74,45 +74,6 @@ public class Lab4 {
 
 		}
 
-		/*
-		 * sportsEquipment.cost = 100;
-		 * 
-		 * sportsEquipment.cost = sportsEquipment.cost + 100;
-		 * 
-		 * sportsEquipment.cost += 100;
-		 * 
-		 * System.out.println(sportsEquipment.cost);
-		 */
-
-//        sportsEquipment.color = "White";
-
-		/*
-		 * System.out.println(sportsEquipment.getColor());
-		 * 
-		 * sportsEquipment.setColor("White", "MySupperPassword");
-		 * 
-		 * System.out.println(sportsEquipment.getColor());
-		 */
-
-		/*
-		 * System.out.println("Done! ;-)^)^))^");
-		 * 
-		 * int a = 1;
-		 * 
-		 * int b = 2;
-		 * 
-		 * boolean c = false;
-		 * 
-		 * if (a) {
-		 * 
-		 * 
-		 * 
-		 * }
-		 * 
-		 * System.out.println(a + b);
-		 * 
-		 * System.out.println(a + c);
-		 */
 
 	}
 
